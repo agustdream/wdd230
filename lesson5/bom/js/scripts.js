e40c8c82c5a326196d2dc906785bf530c1dd26b0
agustdream <gabriela.guterres@gmail.com>
2022-05-19 22:16:52 -0300

    bom top 25 assignment

---
+++ lesson5/bom/js/scripts.js
@@ -0,0 +1,39 @@
+document.querySelector('#addButton').addEventListener('click', addButton);
+
+function addButton() {
+
+  // text box access
+  const userInput = document.getElementById('#favchap');
+  let userInputValue = document.querySelector('#favchap').value;
+  document.querySelector('#favchap').value= ""
+
+
+  if (userInputValue != "") {
+    // ul access
+    const list = document.querySelector('ul');
+
+
+    // li and button
+    const listItem = document.createElement('li');
+
+    // updates list
+    list.appendChild(listItem);
+
+    // loads input to list
+    listItem.textContent = userInputValue;
+
+    // delete button
+    const dltButton = document.createElement("button");
+    dltButton.textContent = "❌";
+
+    listItem.appendChild(dltButton);
+
+    dltButton.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+    
+    userInput.focus();
+  }  
+
+
+}
