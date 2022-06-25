d4fdddda7cee4e4f1587e6a634d0c76c74808ada
agustdream <gabriela.guterres@gmail.com>
2022-06-25 20:39:00 -0300

    first update chamber week 8

--- lesson8/chamber/js/windchill.js
+++ lesson8/chamber/js/windchill.js
@@ -1,12 +1,31 @@
-function calculateWindChill(temperature, windSpeed) {
-    return 35.74 + (0.6215 * temperature) + (0.4275 * temperature - 35.75)  *  windSpeed ^ 0.16;
-  }
+//Values
+var temperature=parseInt(48);
+var windSpeed=parseFloat(4.0);
 
-const temp = 40,
-windSpeed = 5;
+//Output
+let t=document.querySelector("#temperature");
+let s=document.querySelector("#wind");
+let windChill=document.querySelector("#wind-chill");
 
-console.log(
-  `The chill factor for temperature "${temp}" and wind speed "${windSpeed}" is "${calculateWindChill(temp, windSpeed)}".`
-);
+//Wind speed
+const mph=windSpeed/1.60934
+const kmH=windSpeed*1.60934
 
-document.getElementById("windChill").innerHTML = `The chill factor for temperature "${temp}" and wind speed "${windSpeed}" is "${calculateWindChill(temp, windSpeed)}".`
+//Convert Celsius-Farenheit
+const toCelsius=(temperature*9/5)+32;
+const toFarenheit=(temperature-32)*5/9;
+
+//Windchill
+const chill=35.74+(0.6215*temperature)-(35.75*windSpeed**0.16)+(0.4275*temperature*windSpeed**0.16);
+
+
+if (temperature > 50 || windSpeed < 3.0){
+    windChill.innerHTML="<p>N/A</p>";
+} else{
+    windChill.innerHTML=`<p>${chill.toFixed(2)} °F</p>`;
+};
+
+t.innerHTML=`<h2>${temperature} °F</h2>`;
+s.innerHTML=`<p>${windSpeed} mp/h</p>`;
+
+
